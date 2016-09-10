361e4941d3152867f51e4fbbd824f30f14b87967
Roopa <roopamaddikunta1@gmail.com>
2016-09-10 15:41:24 -0500

    initial files

---
+++ Getting+Around+python+.py
@@ -0,0 +1,105 @@
+
+# coding: utf-8
+
+# In[1]:
+
+2+5 
+
+
+# In[2]:
+
+## this is a comment 
+
+
+# so lets see this
+
+# # okay then
+
+# ## that seems very huge 
+# 
+
+# In[3]:
+
+myValue = 2-1
+print myValue
+
+
+# In[4]:
+
+myValue
+
+
+# In[5]:
+
+mylist = ['a', 'b','c','d']
+print mylist
+
+
+# In[6]:
+
+mylist[:1]
+
+
+# In[7]:
+
+mylist[:0]
+
+
+# In[8]:
+
+mylist[0]
+
+
+# In[9]:
+
+mylist[:2]
+
+
+# In[11]:
+
+mylist[:-2]
+
+
+# In[13]:
+
+mylist.append('e')
+
+
+# In[14]:
+
+mylist
+
+
+# In[17]:
+
+def squared(a):
+    " " "returns the squared value of a" " "
+    return a**2
+
+
+# In[18]:
+
+print squared(2)
+
+
+# In[21]:
+
+range(1,10,2)
+
+
+# In[22]:
+
+for i in range(1,10,2):
+    print squared(i)
+
+
+# In[23]:
+
+for i in mylist:
+    print i
+
+
+# In[ ]:
+
+
+
